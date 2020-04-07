ada399a81315144789d3716d65ac0831e0a9e754
Lewis Able Rafuse <rafusel@mcmaster.ca>
2020-04-06 20:20:26 -0400

    Added new game API endpoint.

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-
+import json
 
 game = Blueprint('game', __name__)
 
@@ -8,3 +8,29 @@ from main import db
 @game.route('/')
 def playGame():
     return render_template('game.html')
+
+@game.route('/api/new')
+def new():
+    data = request.json
+    name = data[0]
+    gameState = json.dumps(data[1])
+
+    game = SavedGame(name=name, gameState=gameState)
+
+    db.session.add(game)
+    db.session.commit()
+
+    return "Game created."
+
+
+#@game.route('/api/save')
+#def save():
+#    game = request.json
+    
+
+
+@game.route('/api/load')
+def load():
+    data = request.json
+    game = SavedGame.query.filter_by(id=data).first()
+    return json.loads(game.gameState)
