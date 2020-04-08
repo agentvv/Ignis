ece31827f9f989841af3155edd4e6094cb8b3509
Lewis Able Rafuse <rafusel@mcmaster.ca>
2020-04-08 02:12:36 -0400

    Weather balancing.

--- static/game.js
+++ static/game.js
@@ -31,7 +31,7 @@ function tick() {
         return;
 
     updateWeather();
-    fire.size += 10 - getWeatherEffects();
+    fire.size += getWeatherEffects();
 
     if (fire.colour !== "Default") {
         fire.colourTime--;
